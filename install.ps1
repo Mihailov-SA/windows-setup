409bcdcc7e06df5ef4d2c6ba27d1d3667012d197
Rob Davis <pahimar@gmail.com>
2020-08-28 08:54:27 -0400

    initial scripts

---
+++ install.ps1
@@ -0,0 +1,31 @@
+#Requires -RunAsAdministrator
+
+# TODO Set up self elevation
+# TODO Work in debloat step
+# * https://github.com/Sycnex/Windows10Debloater
+# * https://github.com/farag2/Windows-10-Setup-Script
+
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# install chocolatey if not installed
+Write-Host "⚙️ Checking for Chocolatey"
+if (!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
+    Write-Host "Chocolatey not found, installing chocolatey"
+    Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    Write-Host "✔️ Chocolatey installed"
+}
+else {
+    Write-Host "✔️ Chocolatey found"
+}
+
+Write-Host "⚙️ Setting up Windows features"
+Get-Content ".\windows-features.txt" | ForEach-Object { ($_ -split "\r\n")[0] } | ForEach-Object {
+    Write-Host "📦 Setting up DSIM package: $_"
+    choco install $_ /y /source windowsfeatures
+}
+
+Write-Host "⚙️ Installing applications"
+Get-Content ".\applications.txt" | ForEach-Object { ($_ -split "\r\n")[0] } | ForEach-Object {
+    Write-Host "📦 Installing package: $_"
+    choco install $_ /y
+}
---
+++ setup.ps1
@@ -0,0 +1,26 @@
+#Requires -RunAsAdministrator
+
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# install chocolatey if not installed
+Write-Host "⚙️ Checking for Chocolatey"
+if (!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
+    Write-Host "Chocolatey not found, installing chocolatey"
+    Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    Write-Host "✔️ Chocolatey installed"
+}
+else {
+    Write-Host "✔️ Chocolatey found"
+}
+
+# Write-Host "⚙️ Setting up Windows features"
+# Get-Content ".\windows-features.txt" | ForEach-Object { ($_ -split "\r\n")[0] } | ForEach-Object {
+#     Write-Host "📦 Setting up DSIM package: $_"
+#     choco install $_ /y /source windowsfeatures
+# }
+
+# Write-Host "⚙️ Installing applications"
+# Get-Content ".\applications.txt" | ForEach-Object { ($_ -split "\r\n")[0] } | ForEach-Object {
+#     Write-Host "📦 Installing package: $_"
+#     choco install $_ /y
+# }
